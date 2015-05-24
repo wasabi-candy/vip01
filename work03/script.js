d13b79564f187cba28a07ea798ddf7faf9e329ad
user <user@test.com>
2015-05-25 00:48:09 +0900

    test

--- work03/script.js
+++ work03/script.js
@@ -1,4 +1,18 @@
-(function(){
+/*
+ *	夜食にカップ焼きそば食べたいな
+ *	普通の焼きそばと、カップ焼きそばとでは、食べたいタイミングが少し違う
+ *	これがカップ焼きそば現象
+ * */
 
+window.onload = function(){
 
-})();
+	var display = document.getElementById("window");
+	for(var i=0;i<10;i++){
+
+		(function(a){
+			document.getElementById("num"+i).onclick = function(){
+				display.innerHTML = a;
+			}
+		})(i);
+	}
+}
