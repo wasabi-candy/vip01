9ee6b00d6e4c13b6c395d9811ba6c7d900409875
user <user@test.com>
2015-05-27 20:22:42 +0900

    test

--- work05/script.js
+++ work05/script.js
@@ -28,7 +28,7 @@
         "0000011011000000",
         "0000001001110000"
             ];
-    
+
 
 
     window.onload = function(){
@@ -37,17 +37,34 @@
         var timer = setInterval(loop,1000);
     }
 
+    //キーボード入力
+    document.onkeydown = function(e){
+        if(new_flag)return 0;
+
+        if(e.keyCode == "38"){
+            current_mino.rotation();
+        }else if(e.keyCode == "37"){
+            current_mino.left();
+
+        }else if(e.keyCode == "39"){
+            current_mino.right();
+        }else if(e.keyCode == "40"){
+            current_mino.down();
+
+        }
+    }
+
     //ゲームのタイマー関数
     //一秒ごとに実行されるよ！
     function loop(){
 
         if(new_flag){
-        //ミノを新しく生成するとき用
+            //ミノを新しく生成するとき用
             new_flag = false;
             current_mino = new Mino(counter);
             counter++;
         }else{
-        //ミノを落下させるとき用
+            //ミノを落下させるとき用
             current_mino.fall();
             if(current_mino.onFloor()){
                 new_flag = true;
@@ -63,6 +80,9 @@
 
     //最小ブロックを操作してミノとして振る舞わせるためのクラス
     function Mino(num){
+        var top_position = 0;
+        var left_position = 8;
+        var state = 0;
         var mino_materials = new Array(4);
         var color = colors[Math.ceil(Math.random()*4)];
         var type = mino_type[Math.ceil(Math.random()*6)];
@@ -72,7 +92,7 @@
                 for(var j = 0;j<4;j++){
                     if(type.charAt(i*4+j)=='1'){
                         mino_materials[c] = new Material();
-                        mino_materials[c].setPoints(150+(j*20),i*20);
+                        mino_materials[c].setPoints(left_position+(j),i);
                         mino_materials[c].setColor(color);
                         tetris.appendChild(mino_materials[c].getObj());
                         c++;
@@ -81,15 +101,55 @@
                 }
             }
         })();
-        
+
         this.fall = function(){
+            top_position++;
             for(var i=0;i<4;i++){
                 var temp = mino_materials[i];
-                temp.setPoints(temp.getX(),temp.getY()+20);
+                temp.setPoints(temp.getX(),temp.getY()+1);
             }
         }
         this.onFloor = function(){
             return false;
+        }
+        this.left = function(){
+            left_position--;
+            for(var i=0;i<4;i++){
+                var temp = mino_materials[i];
+                temp.setPoints(temp.getX()-1,temp.getY());
+            }
+        }
+        this.right = function(){
+            left_position++;
+            for(var i=0;i<4;i++){
+                var temp = mino_materials[i];
+                temp.setPoints(temp.getX()+1,temp.getY());
+            }
+        }
+        this.rotation = function(){
+            var temp = "";
+            for(var i = 0;i<4;i++){
+                for(var j = 0;j<4;j++){
+                    temp += type.charAt(j*3+i);
+                }
+            }
+            type = temp;
+            mapping(temp);
+        }
+        function mapping(type){
+            var c = 0;
+            for(var i = 0;i<4;i++){
+                for(var j = 0;j<4;j++){
+                    if(type.charAt(i*4+j)=='1'){
+                        var temp = mino_materials[c];
+                        temp.setPoints(left_position+j,top_position+i);
+                        c++;
+                    }
+
+                }
+            }
+
+
         }
 
     }
@@ -114,8 +174,8 @@
         this.setPoints = function(tx,ty){
             x = tx;
             y = ty;
-            mat.style.marginLeft = x+"px";
-            mat.style.marginTop = y+"px";
+            mat.style.marginLeft = (x*20)+"px";
+            mat.style.marginTop = (y*20)+"px";
         }
         this.getX = function(){
             return x;
