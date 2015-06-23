bc3562e47c0a5e61f7286163d3554ce6a6fc38cd
user <user@test.com>
2015-06-23 22:17:18 +0900

    t

--- work06/script.js
+++ work06/script.js
@@ -1,5 +1,9 @@
 window.onload = function(){
-    ctx = document.getElementById("c").getContext("2d");
+    
+    var ele = document.getElementById("c");
+    ele.setAttribute("width","500px");
+    ele.setAttribute("height","500px");
+    var ctx = ele.getContext("2d");
     ctx.beginPath();
     ctx.fillRect(10,10,10,10);
 }
